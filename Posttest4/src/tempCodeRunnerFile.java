d475f9c1a0f3a5e2fc69204ae241e72e22dc0a1e
Yuyun <161832816+YuyunNabila@users.noreply.github.com>
2025-04-14 23:55:26 +0800

    Delete Posttest4 directory

--- Posttest4/src/tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-        System.out.printf("%-10s %-20s %-15s Rp%,12d %-15s%n", 
-                b.getId(), b.getNama(), b.getKategori(), b.getHargaAwal(), namaPemilik);
-        }
-    
-        System.out.println("---------------------------------------------------------------------------------");
-    }
